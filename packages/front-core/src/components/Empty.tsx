eb62613ace66fa70b72bbb2d2501285beed106dc
Mandrak-Kimigo <108131079+Mandrak-Kimigo@users.noreply.github.com>
2023-07-14 15:17:53 +0200

    Suppression empty.png

--- packages/front-core/src/components/Empty.tsx
+++ packages/front-core/src/components/Empty.tsx
@@ -8,7 +8,6 @@ interface EmptyProps {
 export function Empty({ text }: PropsWithChildren<EmptyProps>) {
   return (
     <Box p={2} maxHeight={300} textAlign="center">
-      <img src="/img/empty.png" alt={text} />
       <Typography component="p" gutterBottom color="#AAA">
         {text}
       </Typography>
