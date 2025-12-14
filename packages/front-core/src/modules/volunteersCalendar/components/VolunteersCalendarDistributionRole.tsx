196f320ec2b503f9750ce07bc589a2aa35e96561
Mandrak-Kimigo <amaury.cormier@gmail.com>
2025-12-14 14:35:31 +0100

    Correction bug TypeError: t.hasVolunteerRole.get is not a function Lors de l'affichage du calendrier de permanences depuis l'onglet distributions

--- packages/front-core/src/modules/volunteersCalendar/components/VolunteersCalendarDistributionRole.tsx
+++ packages/front-core/src/modules/volunteersCalendar/components/VolunteersCalendarDistributionRole.tsx
@@ -31,10 +31,11 @@ const VolunteersCalendarDistributionRole = ({
   const from = new Date();
   const to = new Date();
 
-  const hasVolunteerRole = multiDistrib.hasVolunteerRole.get(roleId);
-  const volunteer = hasVolunteerRole
-    ? multiDistrib.volunteerForRole.get(roleId)
-    : undefined;
+	const roleKey = String(roleId);
+	const hasVolunteerRole = !!multiDistrib.hasVolunteerRole?.[roleKey];
+	const volunteerForRole = hasVolunteerRole
+		? multiDistrib.volunteerForRole?.[roleKey] ?? undefined
+		: undefined;
 
   let sxColors: SxProps<Theme> = {
     bgcolor: 'initial',
