d915f9a66570b6a237ab6e2d954597d6e749297b
Renaud Bardet <renaud.bardet@lonestone.io>
2026-07-10 11:56:23 +0200

    fix taille de l'image dans le mail

--- packages/front-core/src/modules/messages/editor/nodes/EmailImageStatic.tsx
+++ packages/front-core/src/modules/messages/editor/nodes/EmailImageStatic.tsx
@@ -1,8 +1,42 @@
-import { MessageImageElement } from "../messageEditorSchema";
+import type { HTMLAttributes } from 'react';
+import type { MessageImageElement } from '../messageEditorSchema';
+import {
+    getMessageImageAlignStyles,
+    MESSAGE_IMAGE_PADDING_Y,
+    messageImageImgStyle,
+} from '../plateStyles';
 
-export default function EmailImageStatic({ element }: { element: MessageImageElement }) {
+type Props = {
+    element: MessageImageElement;
+    attributes?: HTMLAttributes<HTMLDivElement>;
+};
 
+export default function EmailImageStatic({ element, attributes }: Props) {
     const src = element.cid ?? (typeof element.url === 'string' ? element.url : '');
+    const alignStyles = getMessageImageAlignStyles(element.align);
 
-    return <img src={src} alt={element.filename} />
+    const filteredAttributes = Object.fromEntries(
+        Object.entries(attributes ?? {})
+            .filter((att) => !att[0].startsWith('data-slate-')),
+    );
+
+    return (
+        <div
+            {...filteredAttributes}
+            className={attributes?.className?.replace('slate-img', '')}
+            style={{
+                ...attributes?.style,
+                paddingTop: MESSAGE_IMAGE_PADDING_Y,
+                paddingBottom: MESSAGE_IMAGE_PADDING_Y,
+                display: 'flex',
+                justifyContent: alignStyles.justifyContent,
+            }}
+        >
+            <img
+                src={src}
+                alt={element.filename ?? ''}
+                style={messageImageImgStyle}
+            />
+        </div>
+    );
 }
